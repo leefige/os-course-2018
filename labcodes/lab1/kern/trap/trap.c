c19940cb6711f2b1c46f07623f53d4347bb703b5
leefige <templar_git@163.com>
2018-03-14 01:53:05 +0800

    #1 Try c1.

--- labcodes/lab1/kern/trap/trap.c
+++ labcodes/lab1/kern/trap/trap.c
@@ -149,6 +149,8 @@ static void
 trap_dispatch(struct trapframe *tf) {
     char c;
 
+    trapframe tf_k_u, tf_u_k;
+
     switch (tf->tf_trapno) {
     case IRQ_OFFSET + IRQ_TIMER:
         /* LAB1 2015010062 : STEP 3 */
@@ -178,6 +180,22 @@ trap_dispatch(struct trapframe *tf) {
         break;
     //LAB1 CHALLENGE 1 : 2015010062 you should modify below codes.
     case T_SWITCH_TOU:
+        tf_k_u = *tf;
+        tf_k_u.tf_cs = USER_CS;
+        tf_k_u.tf_ds = USER_DS;
+        tf_k_u.tf_es = USER_DS;
+        tf_k_u.tf_ss = USER_DS;
+
+        tf_k_u.tf_esp = (uint32_t)tf + sizeof(struct trapframe);// - 8;
+		
+        // set eflags, make sure ucore can use io under user mode.
+        // if CPL > IOPL, then cpu will generate a general protection.
+        tf_k_u.tf_eflags |= FL_IOPL_MASK;
+    
+        // set temporary stack
+        // then iret will jump to the right stack
+        *((uint32_t *)tf - 1) = (uint32_t)&tf_k_u;
+        
     case T_SWITCH_TOK:
         panic("T_SWITCH_** ??\n");
         break;
