ce0131e898ad856ef952161abb0b0375adc20dcc
李逸飞 <liyifei15@mails.tsinghua.edu.cn>
2018-05-03 18:19:07 +0800

    #6 Able to make.

--- labcodes/lab6/kern/process/proc.c
+++ labcodes/lab6/kern/process/proc.c
@@ -139,7 +139,7 @@ alloc_proc(void) {
         list_init(&(proc->run_link));
         proc->time_slice = 0;
         // for stride
-        skew_heap_init((proc->lab6_run_pool));
+        skew_heap_init(&(proc->lab6_run_pool));
         proc->lab6_stride = 0;
         proc->lab6_priority = 0;
 
